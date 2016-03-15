7ed88c46736aa9618373575d6b0d9eea66571133
Wito Delnat <wito.delnat@gmail.com>
2016-03-15 11:31:15 +0100

    Added 7, fixed 6

--- Athens_exercise7.m
+++ Athens_exercise7.m
@@ -1,9 +1,8 @@
 P0 = 0.4;
 P1 = 0.6;
-precomputedGammas = [2.0400, 1.6913, 1.4873];
 
 for i = 3 : -1 : 1
     C = [0, i; 2, 0];
-    [gamma, Pd, Pf, Risk] = Athens_core(P0, P1, C, 0, precomputedGammas(i));
+    [gamma, Pd, Pf, Risk] = Athens_core(P0, P1, C, 0, 0);
     disp(sprintf(repmat('%1.4f\t', 1, 4), gamma, Pd, Pf, Risk));
 end
